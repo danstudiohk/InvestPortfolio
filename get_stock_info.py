94c0bf4a34894cf5fb32cfa0c734ad19aba380e6
Daniel Chan <chumchan2@gmail.com>
2020-09-12 18:05:10 +0800

    Update get_stock_info.py

--- get_stock_info.py
+++ get_stock_info.py
@@ -7,5 +7,5 @@ def get_stock_quote(stock_code):
 
 	ticker = yf.Ticker(stock_code)
 	df_ticker_info = pd.DataFrame.from_dict(ticker.info, orient='index', columns=['values']).reset_index()
-	stock_quote = df_ticker_info[df_ticker_info['index'] == 'regularMarketPrice'].iloc[0]['values']
+	stock_quote = df_ticker_info[df_ticker_info['index'] == 'regularMarketPrice'].values[0,1]
 	return stock_quote
